1f444ad588be725382891f92a2949598a87df700
robertengel <akafallenhero@googlemail.com>
2019-07-02 10:50:51 +0200

    Added Update Message for PWAs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { SwUpdate } from '@angular/service-worker';
+import { HelperService } from '../app/services/helper/helper.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,9 +15,19 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private swUpdate: SwUpdate,
+    private helperService: HelperService,
   ) {
     this.initializeApp();
+  }
+
+  async ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        this.helperService.showUpdateToast();
+      });
+    }
   }
 
   initializeApp() {
